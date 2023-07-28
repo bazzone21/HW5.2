2932ee48703959506c2ec835c4c12411e19818d5
[Vasily Shabrov] <[bazzone21@gmail.com]>
2023-07-28 07:47:31 +0200

    add task score

---
+++ task_score.js
@@ -0,0 +1,14 @@
+let intro = 14;
+let git = 25;
+let js = 69;
+
+let sum = (intro + git + js) / 3;
+
+console.log(sum);
+
+studentName = "Vladimir";
+studentSurname = "Petrov";
+
+console.log(
+  `Уважаемый ${studentName} ${studentSurname} Ваш средний бал равен ${sum}`
+);
