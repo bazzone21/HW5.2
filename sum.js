8d8be0b5c41dfbeadcbf7770052f1be715128fc6
[Vasily Shabrov] <[bazzone21@gmail.com]>
2023-08-02 00:23:34 +0200

    hw5.5 add comment

--- sum.js
+++ sum.js
@@ -3,4 +3,12 @@ let value2 = Math.floor(Math.random() * 10);
 console.log(value2);
 console.log(value1 + value2 / 5 + 17);
 
+// Output
+
+// [Running] node "/Users/vasilijsabrov/Documents/HW5.2/tempCodeRunnerFile.js"
+// 9
+// 30.8
+
+// [Done] exited with code=0 in 0.138 seconds
+
 // Приоритет операторов JS аналогичен приоритету в математических вычислениях
