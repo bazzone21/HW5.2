be600617655a3f39b9ff6d5fd6b9d40e985df9f2
[Vasily Shabrov] <[bazzone21@gmail.com]>
2023-08-02 00:20:28 +0200

    hw5.5 add sum

---
+++ sum.js
@@ -0,0 +1,6 @@
+let value1 = 12;
+let value2 = Math.floor(Math.random() * 10);
+console.log(value2);
+console.log(value1 + value2 / 5 + 17);
+
+// Приоритет операторов JS аналогичен приоритету в математических вычислениях
